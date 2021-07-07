a75b0719bd21427ebaf4b00157e3daf6f39e2ed6
Stephanie Vizzi <stephanie@smartlogic.io>
2021-07-06 22:18:43 -0400

    Winners display: add check for content on load / custom CSS grid

--- assets/client/src/components/challenge_tabs/Winners.js
+++ assets/client/src/components/challenge_tabs/Winners.js
@@ -36,20 +36,21 @@ export const Winners = ({challenge, phaseWinners, print}) => {
     return Object.keys(phaseWinners).map((value, idx) => {
       const phaseWinner = phaseWinners[value]
       console.log({phaseWinner})
-      console.log(Object.keys(phaseWinner).length)
-      console.log(Object.keys(phaseWinner.winners).length)
-      return (
-        <div key={phaseWinner.id || idx } className="card">
-          <div className="card-body ql-editor">
-            {renderOverviewImage(phaseWinner)}
-            <h1 className="my-3">{phaseWinner.phase_title}</h1>
-            <div className="my-3" dangerouslySetInnerHTML={{ __html: phaseWinner.overview }}></div>
-            {phaseWinner.winners && phaseWinner.winners.length >= 1 &&
-              <div className="detail-section winner-grid">{renderWinners(phaseWinner.winners)}</div>
-            }
+      console.log("phase winner length", Object.keys(phaseWinner).length)
+      if (phaseWinner && Object.keys(phaseWinner).length >= 1 ) {
+        return (
+          <div key={phaseWinner.id || idx } className="card">
+            <div className="card-body ql-editor">
+              {renderOverviewImage(phaseWinner)}
+              <h1 className="my-3">{phaseWinner.phase_title}</h1>
+              <div className="my-3" dangerouslySetInnerHTML={{ __html: phaseWinner.overview }}></div>
+              {phaseWinner.winners && phaseWinner.winners.length >= 1 &&
+                <div className="winners-section">{renderWinners(phaseWinner.winners)}</div>
+              }
+            </div>
           </div>
-        </div>
-      )
+        )
+      }
     })
   }
 
